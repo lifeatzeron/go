52ed70a0859b93437cdeb321398e3a8f0ab5b6d4
Sagar Panwar <sagarpanwar0123@gmail.com>
2026-03-01 08:51:41 +0530

    more echo cmd line

--- demo.go
+++ demo.go
@@ -3,14 +3,28 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	var s, sep string
 
+	// Version 1
 	for i := 1; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
 		sep = " "
 	}
 	fmt.Println(s)
+
+	// Version 2: anothor form of the for loop iterates over a range of values from a data type like a string or a slice
+
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+
+	fmt.Println(s)
+
+	// Version 3: This is clearer, faster, and avoids manual state management.
+	fmt.Println(strings.Join(os.Args[1:], " "))
 }
